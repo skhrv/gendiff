46c4694801cffdd2b8d8804f799b8b28ddbeedbe
Ruslan Sukharev <notebook1v@gmail.com>
2018-08-28 00:22:35 +0500

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,16 +3,16 @@ import genDiff from '../src';
 
 describe('genDiff', () => {
   it('json', () => {
-    const expected = fs.readFileSync('__test__/__fixtures__/expected.test1.txt', 'utf-8');
-    const pathToFile1 = '__test__/__fixtures__/before.json';
-    const pathToFile2 = '__test__/__fixtures__/after.json';
+    const expected = fs.readFileSync('__tests__/__fixtures__/expected.test1.txt', 'utf-8');
+    const pathToFile1 = '__tests__/__fixtures__/before.json';
+    const pathToFile2 = '__tests__/__fixtures__/after.json';
     const actual = genDiff(pathToFile1, pathToFile2);
     expect(actual).toBe(expected);
   });
   it('yaml', () => {
-    const expected = fs.readFileSync('__test__/__fixtures__/expected.test1.txt', 'utf-8');
-    const pathToFile1 = '__test__/__fixtures__/before.yaml';
-    const pathToFile2 = '__test__/__fixtures__/after.yaml';
+    const expected = fs.readFileSync('__tests__/__fixtures__/expected.test1.txt', 'utf-8');
+    const pathToFile1 = '__tests__/__fixtures__/before.yaml';
+    const pathToFile2 = '__tests__/__fixtures__/after.yaml';
     const actual = genDiff(pathToFile1, pathToFile2);
     expect(actual).toBe(expected);
   });
