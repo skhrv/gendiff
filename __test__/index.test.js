6ec0d3a3e8e0785a7b389bf28176f1d85a9dca17
Ruslan Sukharev <notebook1v@gmail.com>
2018-08-27 22:44:46 +0500

    fix index.test.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,18 +2,17 @@ import fs from 'fs';
 import genDiff from '../src';
 
 describe('genDiff', () => {
-  const expected = fs.readFileSync('__test__/__fixtures__/expected.test1.txt', 'utf-8');
-  let pathToFile1;
-  let pathToFile2;
   it('json', () => {
-    pathToFile1 = '__test__/__fixtures__/before.json';
-    pathToFile2 = '__test__/__fixtures__/after.json';
+    const expected = fs.readFileSync('__test__/__fixtures__/expected.test1.txt', 'utf-8');
+    const pathToFile1 = '__test__/__fixtures__/before.json';
+    const pathToFile2 = '__test__/__fixtures__/after.json';
     const actual = genDiff(pathToFile1, pathToFile2);
     expect(actual).toBe(expected);
   });
   it('yaml', () => {
-    pathToFile1 = '__test__/__fixtures__/before.yaml';
-    pathToFile2 = '__test__/__fixtures__/after.yaml';
+    const expected = fs.readFileSync('__test__/__fixtures__/expected.test1.txt', 'utf-8');
+    const pathToFile1 = '__test__/__fixtures__/before.yaml';
+    const pathToFile2 = '__test__/__fixtures__/after.yaml';
     const actual = genDiff(pathToFile1, pathToFile2);
     expect(actual).toBe(expected);
   });
