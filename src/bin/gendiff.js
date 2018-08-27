93e84457229e83a242bd145b1230f28e7f44022d
Ruslan Sukharev <notebook1v@gmail.com>
2018-08-28 00:12:22 +0500

    fix gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,8 +8,8 @@ commander
   .description(description)
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    // eslint-disable-next-line no-console
+    console.log(gendiff(firstConfig, secondConfig));
+  })
   .parse(process.argv);
-const [,, path1, path2] = process.argv;
-
-// eslint-disable-next-line no-console
-console.log(gendiff(path1, path2));
