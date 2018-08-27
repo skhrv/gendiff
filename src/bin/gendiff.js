2fd9d0449e560eed2edfb9bd659918f845bc01f6
Ruslan Sukharev <notebook1v@gmail.com>
2018-08-27 17:30:36 +0500

    add gendiff for flat .json

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import commander from 'commander';
+import gendiff from '..';
 import { version, description } from '../../package.json';
 
 commander
@@ -8,3 +9,7 @@ commander
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .parse(process.argv);
+const [,, path1, path2] = process.argv;
+
+// eslint-disable-next-line no-console
+console.log(gendiff(path1, path2));
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import __ from 'lodash';
+import fs from 'fs';
+
+const getFileFromPath = pathToFile => JSON.parse(fs.readFileSync(pathToFile));
+
+const parsing = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const astData1 = keys1.reduce((acc, key) => {
+    if (__.has(data2, key) && data1[key] === data2[key]) {
+      return [...acc, [[key, data1[key]], ' ']];
+    }
+    if (__.has(data2, key)) {
+      return [...acc, [[key, data2[key]], '+'], [[key, data1[key]], '-']];
+    }
+    return [...acc, [[key, data1[key]], '-']];
+  }, []);
+  const result = keys2.reduce((acc, key) => (__.has(data1, key) ? acc : [...acc, [[key, data2[key]], '+']]), astData1);
+  return result;
+};
+const build = (ast) => {
+  const str = ast.reduce((acc, item) => {
+    const [obj, diff] = item;
+    const [key, value] = obj;
+    return `${acc}\n    ${diff} ${key}: ${value}`;
+  }, '');
+  return `{${str}\n  }`;
+};
+
+export default (pathToBefore, pathToAfter) => {
+  const before = getFileFromPath(pathToBefore);
+  const after = getFileFromPath(pathToAfter);
+  const diff = parsing(before, after);
+  return build(diff);
+};
