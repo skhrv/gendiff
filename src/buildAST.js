6f1ddae210e5d0424acad2b1a8a548ddbdf37920
Ruslan Sukharev <notebook1v@gmail.com>
2018-08-28 19:23:11 +0500

    refactor buildAST.js

--- src/buildAST.js
+++ src/buildAST.js
@@ -1,32 +1,34 @@
+
 import _ from 'lodash';
 
+const nodeTypes = [
+  {
+    type: 'unchanged',
+    check: (key, data1, data2) => _.has(data1, key) && _.has(data2, key)
+    && data1[key] === data2[key],
+    process: (key, data1) => [{ key, value: data1[key], type: 'unchanged' }],
+  },
+  {
+    type: 'changed',
+    check: (key, data1, data2) => _.has(data1, key) && _.has(data2, key),
+    process: (key, data1, data2) => [{ key, value: data2[key], type: 'added' }, { key, value: data1[key], type: 'deleted' }],
+  },
+  {
+    type: 'added',
+    check: (key, data1, data2) => !_.has(data1, key) && _.has(data2, key),
+    process: (key, data1, data2) => [{ key, value: data2[key], type: 'added' }],
+  },
+  {
+    type: 'deleted',
+    check: (key, data1, data2) => _.has(data1, key) && !_.has(data2, key),
+    process: (key, data1) => [{ key, value: data1[key], type: 'deleted' }],
+  },
+];
 const buildAST = (data1, data2) => {
-  const nodeTypes = [
-    {
-      type: 'unchanged',
-      check: key => _.has(data1, key) && _.has(data2, key) && data1[key] === data2[key],
-      process: key => [{ key, value: data1[key], type: 'unchanged' }],
-    },
-    {
-      type: 'changed',
-      check: key => _.has(data1, key) && _.has(data2, key),
-      process: key => [{ key, value: data2[key], type: 'added' }, { key, value: data1[key], type: 'deleted' }],
-    },
-    {
-      type: 'added',
-      check: key => !_.has(data1, key) && _.has(data2, key),
-      process: key => [{ key, value: data2[key], type: 'added' }],
-    },
-    {
-      type: 'deleted',
-      check: key => _.has(data1, key) && !_.has(data2, key),
-      process: key => [{ key, value: data1[key], type: 'deleted' }],
-    },
-  ];
   const keys = _.union(Object.keys(data1), Object.keys(data2));
   const result = keys.reduce((acc, key) => {
-    const { process } = nodeTypes.find(({ check }) => check(key));
-    return [...acc, ...process(key)];
+    const { process } = nodeTypes.find(({ check }) => check(key, data1, data2));
+    return [...acc, ...process(key, data1, data2)];
   }, []);
   return result;
 };
