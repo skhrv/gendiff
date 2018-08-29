53fddca65d3982de5ec56df374f659fbcf860edf
Ruslan Sukharev <notebook1v@gmail.com>
2018-08-29 09:52:50 +0500

    add new test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-describe('genDiff', () => {
+describe('flat data', () => {
   it('json', () => {
     const expected = fs.readFileSync('__tests__/__fixtures__/expected.test1.txt', 'utf-8');
     const pathToFile1 = '__tests__/__fixtures__/before.json';
@@ -17,9 +17,18 @@ describe('genDiff', () => {
     expect(actual).toBe(expected);
   });
   it('ini', () => {
-    const expected = fs.readFileSync('__tests__/__fixtures__/expected.test2.txt', 'utf-8');
+    const expected = fs.readFileSync('__tests__/__fixtures__/expected.test1.txt', 'utf-8');
     const pathToFile1 = '__tests__/__fixtures__/before.ini';
     const pathToFile2 = '__tests__/__fixtures__/after.ini';
+    const actual = genDiff(pathToFile1, pathToFile2);
+    expect(actual).toBe(expected);
+  });
+});
+describe('recursive data', () => {
+  it('json', () => {
+    const expected = fs.readFileSync('__tests__/__fixtures__/expected.test2.txt', 'utf-8');
+    const pathToFile1 = '__tests__/__fixtures__/before.test2.json';
+    const pathToFile2 = '__tests__/__fixtures__/after.test2.json';
     const actual = genDiff(pathToFile1, pathToFile2);
     expect(actual).toBe(expected);
   });
