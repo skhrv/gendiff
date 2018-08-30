64f8879c915313417f6060581d9514731921a087
Ruslan Sukharev <notebook1v@gmail.com>
2018-08-30 21:46:17 +0500

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,53 +1,33 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const pathToFixture = filename => `__tests__/__fixtures__/${filename}`;
-
 const typesFile = ['.json', '.yaml', '.ini'];
 
-const filename1Flat = pathToFixture('before');
-const filename2Flat = pathToFixture('after');
-const filename1Recursive = pathToFixture('beforeRecursive');
-const filename2Recursive = pathToFixture('afterRecursive');
-
-const expectedFlatFilePath = pathToFixture('expectedFlat.txt');
-const expectedRecursiveFilePath = pathToFixture('expectedRecursive.txt');
-const expectedPlainFilePath = pathToFixture('expectedPlain.txt');
-const expectedJSONFilePath = pathToFixture('expectedJSON.json');
-
-const iter = (file1, file2, type, option) => {
-  const before = `${file1}${type}`;
-  const after = `${file2}${type}`;
-  return genDiff(before, after, option);
+const test = (ext, type) => {
+  it(`${type} ${ext}`, () => {
+    const before = `__tests__/__fixtures__/before_${type}${ext}`;
+    const after = `__tests__/__fixtures__/after_${type}${ext}`;
+    const expectedFilePath = `__tests__/__fixtures__/expected_${type}.txt`;
+    const actual = genDiff(before, after);
+    const expected = fs.readFileSync(expectedFilePath, 'utf-8');
+    expect(actual).toBe(expected);
+  });
+};
+const testRender = (type) => {
+  it(`${type} render`, () => {
+    const before = '__tests__/__fixtures__/before_recursive.json';
+    const after = '__tests__/__fixtures__/after_recursive.yaml';
+    const expectedFilePath = `__tests__/__fixtures__/expected_${type}.txt`;
+    const expectedFile = fs.readFileSync(expectedFilePath, 'utf-8');
+    const expected = type === 'json' ? JSON.parse(expectedFile) : expectedFile;
+    const actual = genDiff(before, after, type);
+    expect(actual).toEqual(expected);
+  });
 };
 
-describe('flat data', () => {
-  const expectedFlat = fs.readFileSync(expectedFlatFilePath, 'utf-8');
-  const expectedRecursive = fs.readFileSync(expectedRecursiveFilePath, 'utf-8');
-  const expectedPlain = fs.readFileSync(expectedPlainFilePath, 'utf-8');
-  const expectedJSON = JSON.parse(fs.readFileSync(expectedJSONFilePath, 'utf-8'));
-  it('json flat', () => {
-    expect(iter(filename1Flat, filename2Flat, typesFile[0])).toBe(expectedFlat);
-  });
-  it('yaml flat', () => {
-    expect(iter(filename1Flat, filename2Flat, typesFile[1])).toBe(expectedFlat);
-  });
-  it('ini flat', () => {
-    expect(iter(filename1Flat, filename2Flat, typesFile[2])).toBe(expectedFlat);
-  });
-  it('json recursive', () => {
-    expect(iter(filename1Recursive, filename2Recursive, typesFile[0])).toBe(expectedRecursive);
-  });
-  it('yaml recursive', () => {
-    expect(iter(filename1Recursive, filename2Recursive, typesFile[1])).toBe(expectedRecursive);
-  });
-  it('ini recursive', () => {
-    expect(iter(filename1Recursive, filename2Recursive, typesFile[2])).toBe(expectedRecursive);
-  });
-  it('plain render', () => {
-    expect(iter(filename1Recursive, filename2Recursive, typesFile[1], 'plain')).toBe(expectedPlain);
-  });
-  it('json render', () => {
-    expect(iter(filename1Recursive, filename2Recursive, typesFile[1], 'json')).toEqual(expectedJSON);
-  });
+describe('gendiff', () => {
+  typesFile.forEach(ext => test(ext, 'flat'));
+  typesFile.forEach(ext => test(ext, 'recursive'));
+  testRender('plain');
+  testRender('json');
 });
