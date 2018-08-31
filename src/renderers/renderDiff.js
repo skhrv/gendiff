a1dedc585763533c3cebc3d54d6c6bdc3c0ce473
Ruslan Sukharev <notebook1v@gmail.com>
2018-08-31 22:18:38 +0500

    refactor

--- src/renderers/renderDiff.js
+++ src/renderers/renderDiff.js
@@ -19,37 +19,29 @@ const valueToString = (value, deepLevel) => (_.isObject(value)
 const nodeToStr = (key, value, symbol, deepLevel) => `${tab(deepLevel)}${symbol} ${key}: ${value}`;
 
 const nodeTypesForRender = {
-  nest: {
-    toString: ({ key, children }, deepLevel, func) => {
-      const processedValue = func(children, deepLevel);
-      return nodeToStr(key, processedValue, ' ', deepLevel);
-    },
+  nest: ({ key, children }, deepLevel, func) => {
+    const processedValue = func(children, deepLevel);
+    return nodeToStr(key, processedValue, ' ', deepLevel);
   },
-  changed: {
-    toString: ({ key, valueBefore, valueAfter }, deepLevel) => {
-      const processedValueBefore = valueToString(valueBefore, deepLevel);
-      const processedValueAfter = valueToString(valueAfter, deepLevel);
-      return [nodeToStr(key, processedValueBefore, '-', deepLevel),
-        nodeToStr(key, processedValueAfter, '+', deepLevel)];
-    },
+  changed: ({ key, valueBefore, valueAfter }, deepLevel) => {
+    const processedValueBefore = valueToString(valueBefore, deepLevel);
+    const processedValueAfter = valueToString(valueAfter, deepLevel);
+    return [nodeToStr(key, processedValueBefore, '-', deepLevel),
+      nodeToStr(key, processedValueAfter, '+', deepLevel)];
   },
-  added: {
-    toString: ({ key, valueAfter }, deepLevel) => nodeToStr(key, valueToString(valueAfter, deepLevel), '+', deepLevel),
-  },
-  deleted: {
-    toString: ({ key, valueBefore }, deepLevel) => nodeToStr(key, valueToString(valueBefore, deepLevel), '-', deepLevel),
-  },
-  unchanged: {
-    toString: ({ key, valueAfter }, deepLevel) => nodeToStr(key, valueToString(valueAfter, deepLevel), ' ', deepLevel),
-  },
+  added: ({ key, valueAfter }, deepLevel) => nodeToStr(key, valueToString(valueAfter, deepLevel), '+', deepLevel),
+
+  deleted: ({ key, valueBefore }, deepLevel) => nodeToStr(key, valueToString(valueBefore, deepLevel), '-', deepLevel),
+
+  unchanged: ({ key, valueAfter }, deepLevel) => nodeToStr(key, valueToString(valueAfter, deepLevel), ' ', deepLevel),
 };
 
 export default (ast) => {
   const iter = (nodes, deepLevel) => {
     const nodesProccesed = nodes.map((node) => {
       const { type } = node;
-      const nodeActionForRender = nodeTypesForRender[type];
-      return nodeActionForRender.toString(node, deepLevel + 1, iter);
+      const nodeRender = nodeTypesForRender[type];
+      return nodeRender(node, deepLevel + 1, iter);
     });
     const result = _.flattenDeep(nodesProccesed).join('\n');
     return deepLevel === 0
--- src/renderers/renderPlain.js
+++ src/renderers/renderPlain.js
@@ -3,29 +3,24 @@ import _ from 'lodash';
 const valueToPlainStr = value => (_.isObject(value) ? '[complex value]' : value);
 
 const nodeTypesForRender = {
-  nest: {
-    toString: ({ children }, path, func) => func(children, `${path}.`),
-  },
-  changed: {
-    toString: ({ valueBefore, valueAfter }, path) => `Property '${path}' was updated. From '${valueToPlainStr(valueBefore)}' to '${valueToPlainStr(valueAfter)}'`,
-  },
-  added: {
-    toString: ({ valueAfter }, path) => `Property '${path}' was added with value: '${valueToPlainStr(valueAfter)}'`,
-  },
-  deleted: {
-    toString: (node, path) => `Property '${path}' was removed`,
-  },
-  unchanged: {
-    toString: () => '',
-  },
+  nest: ({ children }, path, func) => func(children, `${path}.`),
+  changed: ({ valueBefore, valueAfter }, path) => `Property '${path}' was updated. From '${valueToPlainStr(valueBefore)}' to '${valueToPlainStr(valueAfter)}'`,
+
+  added: ({ valueAfter }, path) => `Property '${path}' was added with value: '${valueToPlainStr(valueAfter)}'`,
+
+  deleted: (node, path) => `Property '${path}' was removed`,
+
+  // unchanged: () => '',
 };
+
 export default (ast) => {
-  const iter = (nodes, path) => nodes.map((node) => {
-    const { key, type } = node;
-    const newPath = `${path}${key}`;
-    const nodeActionForRender = nodeTypesForRender[type];
-    return nodeActionForRender.toString(node, newPath, iter);
-  }, '');
+  const iter = (nodes, path) => nodes.filter(({ type }) => type !== 'unchanged')
+    .map((node) => {
+      const { key, type } = node;
+      const newPath = `${path}${key}`;
+      const nodeRender = nodeTypesForRender[type];
+      return nodeRender(node, newPath, iter);
+    }, '');
   const plainNodes = _.flattenDeep(iter(ast, ''));
-  return _.compact(plainNodes).join('\n');
+  return plainNodes.join('\n');
 };
